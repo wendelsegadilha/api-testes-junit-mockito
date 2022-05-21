494c565aa350f0b31302fda5fae7fb958cb2b99f
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-05-20 22:24:25 -0300

    projeto iniciado

---
+++ src/test/java/xyz/wendelsegadilha/apijunitmockito/ApiJunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.wendelsegadilha.apijunitmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJunitMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
