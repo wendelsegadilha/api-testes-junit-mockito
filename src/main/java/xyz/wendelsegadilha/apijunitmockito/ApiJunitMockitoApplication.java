00ce9e3348f0e0a33f474479366f09d95f355ab7
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-05-21 11:32:35 -0300

    classe user

--- src/main/java/xyz/wendelsegadilha/apijunitmockito/ApiJunitMockitoApplication.java
+++ src/main/java/xyz/wendelsegadilha/apijunitmockito/ApiJunitMockitoApplication.java
@@ -2,12 +2,14 @@ package xyz.wendelsegadilha.apijunitmockito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import xyz.wendelsegadilha.apijunitmockito.domain.User;
 
 @SpringBootApplication
 public class ApiJunitMockitoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiJunitMockitoApplication.class, args);
+
     }
 
 }
