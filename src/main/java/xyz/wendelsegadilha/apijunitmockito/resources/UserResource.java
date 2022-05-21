0b6189095294e5a9985aa818a9c0481fd23920b2
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-05-21 11:59:35 -0300

    classe user resource findById

--- src/main/java/xyz/wendelsegadilha/apijunitmockito/resources/UserResource.java
+++ src/main/java/xyz/wendelsegadilha/apijunitmockito/resources/UserResource.java
@@ -1,19 +1,23 @@
 package xyz.wendelsegadilha.apijunitmockito.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.wendelsegadilha.apijunitmockito.domain.User;
+import xyz.wendelsegadilha.apijunitmockito.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-
-        return ResponseEntity.ok().body(new User(1, "Wendel", "wendel@gmail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
