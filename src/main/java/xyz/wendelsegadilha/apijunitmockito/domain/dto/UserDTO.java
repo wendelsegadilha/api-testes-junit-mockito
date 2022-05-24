619cf02a25ffa793f65c9683324dfccd3720a588
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-05-24 11:17:42 -0300

    removendo anatocoes nao utilizadas

--- src/main/java/xyz/wendelsegadilha/apijunitmockito/domain/dto/UserDTO.java
+++ src/main/java/xyz/wendelsegadilha/apijunitmockito/domain/dto/UserDTO.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 
 @Getter @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
--- src/main/java/xyz/wendelsegadilha/apijunitmockito/resources/exceptions/StandardError.java
+++ src/main/java/xyz/wendelsegadilha/apijunitmockito/resources/exceptions/StandardError.java
@@ -2,12 +2,10 @@ package xyz.wendelsegadilha.apijunitmockito.resources.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class StandardError {
 
